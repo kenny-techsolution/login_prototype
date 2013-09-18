48531c93b86f0412b6699e9c670a41ecd3c76c65
Kenny Chung <yoyocicada@gmail.com>
2013-09-18 15:12:59 -0400

    spring mvc setup

--- src/main/java/com/dovesquare/login_prototype/App.java
+++ src/main/java/com/dovesquare/login_prototype/App.java
@@ -1,13 +1,33 @@
 package com.dovesquare.login_prototype;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+ 
+public class App extends AbstractHandler
 {
-    public static void main( String[] args )
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) 
+        throws IOException, ServletException
     {
-        System.out.println( "Hello World!" );
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>sssssss!</h1>");
+    }
+ 
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new App());
+ 
+        server.start();
+        server.join();
     }
 }
