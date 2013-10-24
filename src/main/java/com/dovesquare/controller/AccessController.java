b284f189ead2983ffd90fcc8965fe7d4b44a2c77
Kenny Chung <yoyocicada@gmail.com>
2013-10-23 21:36:11 -0400

    add connection table sql

--- src/main/java/com/dovesquare/controller/AccessController.java
+++ src/main/java/com/dovesquare/controller/AccessController.java
@@ -1,12 +1,10 @@
 package com.dovesquare.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class AccessController {
-	
+
 }
