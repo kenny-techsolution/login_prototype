fc06ddc2099008fab320d43d2e3b18809e32f096
Kenny Chung <yoyocicada@gmail.com>
2013-10-30 17:59:21 -0400

    config change

--- src/main/java/com/dovesquare/domain/Role.java
+++ src/main/java/com/dovesquare/domain/Role.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 import org.codehaus.jackson.annotate.JsonBackReference;
@@ -17,6 +18,7 @@ public class Role {
 	
 	@JsonBackReference
 	@OneToOne
+	@JoinColumn(name = "user_id")
 	private User user;
 	
 	private Integer role;
